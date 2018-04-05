15cef0729d6614d84251a2a16a7087d890f0f8ca
larrotino <larrotino@email.com>
2018-04-05 23:25:21 +0200

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -4,3 +4,21 @@
   Stampa anche quanti sono i valori monori della media e quanti quelli maggiori.
   http://www.imparareaprogrammare.it
 */
+
+var valori = [1,2,3,6,10,5,77,43,24,65,1,44,6,322,53];
+
+//metodo reduce per addizionare tutto l'array
+var accumulatore = (accumulator, currentValue) => accumulator + currentValue;
+var media = valori.reduce(accumulatore)/valori.length;
+
+
+//metodo filter crea una variabile con solo gli elementi filtrati
+var filtrati = valori.filter(
+  function evenNumbers (valore) {
+    return valore < media;
+  }
+)
+
+console.log(filtrati);
+console.log(filtrati.length+'quantità valori minori alla media');
+console.log(Math.abs(filtrati.length-valori.length)+'quantità valori minori alla media');
