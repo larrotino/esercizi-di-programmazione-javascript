f2ad97e0e0d47d805f12de5c7c5e2217f175f46a
larrotino <larrotino@email.com>
2018-04-05 15:21:21 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -14,3 +14,34 @@
   Utilizza il costrutto Switch-Case mediante acquisizione di caratteri.
   http://www.imparareaprogrammare.it
 */
+
+var input1 = 15;
+var input2 = 2;
+
+var operatore = '+';
+
+switch (operatore) {
+  case '+':
+    console.log(input1+input2);
+    break;
+  case '-':
+    console.log(input1-input2);
+    break;
+  case '*':
+    console.log(input1*input2);
+    break;
+  case '/':
+    console.log(input1/input2);
+    break;
+  case '%':
+    console.log(input1%input2);
+    break;
+  case '^':
+    console.log(input1**input2);
+    break;
+  case 'm':
+    console.log((input1+input2)/2);
+    break;
+  default:
+    console.log('impossibile!');
+}
