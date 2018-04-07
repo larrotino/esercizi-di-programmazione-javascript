3a91a4eadf5c1f7ed769cefa4dd9590efbf09cf3
larrotino <larrotino@email.com>
2018-04-07 17:17:43 +0200

    Update pari-o-dispari.js

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -3,3 +3,10 @@
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
 */
+var numero = 0;
+
+if(Number.isInteger(numero/2)){
+	console.log(1)
+}else{
+  console.log(0)
+}
