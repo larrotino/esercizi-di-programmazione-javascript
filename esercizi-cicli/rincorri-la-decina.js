954d654e090104992383fbcabb814ec9ac65953a
larrotino <larrotino@email.com>
2018-04-07 17:18:33 +0200

    Update rincorri-la-decina.js

--- esercizi-cicli/rincorri-la-decina.js
+++ esercizi-cicli/rincorri-la-decina.js
@@ -4,3 +4,18 @@
   Hint: per andare a capo stampa '\n'.
   http://www.imparareaprogrammare.it
 */
+
+var num = 5;
+var lettera = 'X';
+var cicli = 1;
+
+while (lettera.length >= num) {
+    console.log(lettera+cicli);
+  cicli+2;
+}
+
+while (lettera.length = 1) {
+  cicli-2;
+  console.log(lettera-cicli);
+
+}
