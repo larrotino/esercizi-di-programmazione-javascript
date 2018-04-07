4458b338e6b3169b3a0f29eafb987b72066a3836
larrotino <larrotino@email.com>
2018-04-07 17:19:33 +0200

    Update invertimi.js

--- esercizi-array/invertimi.js
+++ esercizi-array/invertimi.js
@@ -6,3 +6,25 @@
   Non utilizzare array di appoggio per inversione.
   http://www.imparareaprogrammare.it
 */
+var valori = [1,2,3,6,10,5,77,43,24,65,1,44,6,322,53];
+
+//metodo reduce per addizionare tutto l'array
+var accumulatore = (accumulator, currentValue) => accumulator + currentValue;
+var media = valori.reduce(accumulatore)/valori.length;
+
+
+//metodo filter crea una variabile con solo gli elementi filtrati
+var filtrati = valori.filter(valore=>valore<media)
+
+/*
+//vecchia formula
+
+var filtrati = valori.filter(
+  function evenNumbers (valore) {
+    return valore < media;
+  }
+)
+*/
+console.log(filtrati);
+console.log(filtrati.length+'quantità valori minori alla media');
+console.log(Math.abs(filtrati.length-valori.length)+'quantità valori minori alla media');
