d002266e54b2367e5fc9354fb0d4f333a4e2ea26
larrotino <larrotino@email.com>
2018-04-07 17:17:18 +0200

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -10,3 +10,21 @@
   Se la scelta non è tra quelle elencate verrà stampata la scritta 'Dolce non disponibile' altrimenti verrà visualizzata la scelta effettuata 'Hai scelto il dolce X'.
   http://www.imparareaprogrammare.it
 */
+var dolce1 = 'tiramisu';
+var dolce2 = 'torta della nonna';
+var dolce3 = 'Cheesecake alla nutella';
+var dolce4 = 'Macedonia';
+
+var scelta = 2;
+
+if (scelta == 1){
+  console.log(`hai scelto il ${dolce1}`);
+}else if (scelta == 2) {
+  console.log(`hai scelto la ${dolce2}`);
+}else if (scelta == 3) {
+  console.log(`hai scelto la ${dolce3}`);
+}else if (scelta == 4) {
+  console.log(`hai scelto la ${dolce4}`);
+} else {
+  console.log('dolce non disponibile');
+}
