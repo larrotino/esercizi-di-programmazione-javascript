9875a4e26cfbdba0be491353419a8126479bcbc7
larrotino <larrotino@email.com>
2018-04-07 17:12:46 +0200

    Update conta-il-tempo.js

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,13 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+
+var catsnumbers = 44;
+var catsinarow = 6;
+
+var row = Math.ceil(catsnumbers/catsinarow);
+var lostcats = row*catsinarow-catsnumbers;
+
+console.log("le colonne di gatti sono" + row);
+console.log("mentre i gatti non in fila sono" + lostcats);
+console.log(`le colonne dei gatti sono ${row} mentre i posti liberi sono ${lostcats}`);
