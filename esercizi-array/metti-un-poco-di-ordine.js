628de59e79f1c2e9fe33b81290da9ebfa55e5f66
larrotino <larrotino@email.com>
2018-04-17 16:22:49 +0200

    Update metti-un-poco-di-ordine.js

--- esercizi-array/metti-un-poco-di-ordine.js
+++ esercizi-array/metti-un-poco-di-ordine.js
@@ -6,3 +6,25 @@
   Prova ad ordinali in modo crescente.
   http://www.imparareaprogrammare.it
 */
+
+
+var numeri = [5,10,8,41,87,2,9,62,44];
+var ordinati = [];
+
+
+for (var l = 0; l < numeri.length; l++) {
+
+
+  for (var i = 0; i < ordinati.length; i++) {
+    if (numeri[l] > ordinati[i] && numeri[l] < ordinati [i+1]) {
+      // inserisci numeri[l] dopo ordinati [i]
+    } else if (numeri[l] < ordinati[i] && numeri[l] > ordinati [i-1]) {
+      // inserisci numeri[l] prima di ordinati [i]
+    }
+  }
+
+
+}
+
+
+console.log(ordinati);
