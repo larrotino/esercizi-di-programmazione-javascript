99bd98b98f0834dad44af913b900dc1db623f944
larrotino <larrotino@email.com>
2018-04-18 10:12:14 +0200

    Update procedi-alla-cieca.js

--- esercizi-array-multidimensionali/procedi-alla-cieca.js
+++ esercizi-array-multidimensionali/procedi-alla-cieca.js
@@ -4,3 +4,19 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var arraymulti = [];
+
+arraymulti[0] = new Array(5);
+arraymulti[1] = new Array(5);
+arraymulti[2] = new Array(5);
+
+for (var l = 0; l < arraymulti.length; l++) {
+
+  for (var i = 0; i < arraymulti[l].length; i++) {
+    arraymulti[l][i] =  Math.floor(Math.random() * 50)
+  }
+  
+}
+
+console.log(arraymulti);
